979ac2a7dfed31db39639fdd7d6e3bc7084ebc4d
steviekaye <stevie.kaye@gmail.com>
2018-12-11 19:56:13 +1300

    Tidied up legacy controller/route name issues

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to category_path
+      redirect_to categories_path
     else
       render 'index'
     end
--- app/controllers/subcategories_controller.rb
+++ app/controllers/subcategories_controller.rb
@@ -2,14 +2,15 @@ class SubcategoriesController < ApplicationController
   def create
     @category = Category.find(params[:category_id])
     @subcategory = @category.subcategories.create(subcategory_params)
-    redirect_to category_path
+
+    redirect_to categories_path
   end
 
   def destroy
-    @category = Category.find(params[:category_id])
-    @subcategory = @category.subcategories.find(params[:id])
+    @subcategory = Subcategory.find(params[:id])
     @subcategory.destroy
-    redirect_to category_path
+
+    redirect_to categories_path
 end
 
   private
