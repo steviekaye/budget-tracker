a2b8781fed69b0d4a1fe77f5f1cd2effebcdd777
steviekaye <stevie.kaye@gmail.com>
2019-01-29 15:43:26 +1300

    Chartkick usage

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -3,5 +3,14 @@ class SummariesController < ApplicationController
     @categories = Category.all
     @date_start = Purchase.minimum(:date)
     @date_end = Date.today
+    @totals = category_totals
+  end
+
+  def category_totals
+    @totals = []
+    Category.all.each do |c|
+      @totals << [c.name, Purchase.where(subcategory: c.subcategories).to_a.map(&:amount).sum]
+    end
+    @totals
   end
 end
