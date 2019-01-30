202708575ce922e5947fe4dcdcc4606608945048
steviekaye <stevie.kaye@gmail.com>
2019-01-30 14:38:14 +1300

    Tweaks as per PR

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -1,16 +1,21 @@
 class SummariesController < ApplicationController
   def index
-    @categories = Category.all
-    @date_start = Purchase.minimum(:date)
-    @date_end = Date.today
     @totals = category_totals
+    @overall_sum = Purchase.all.to_a.map(&:amount).sum
   end
 
+  private
+
   def category_totals
-    @totals = []
-    Category.all.each do |c|
-      @totals << [c.name, Purchase.where(subcategory: c.subcategories).to_a.map(&:amount).sum]
+    Category.all.map do |c|
+      [
+        c.name,
+        sum_purchases_in_category(c)
+      ]
     end
-    @totals
+  end
+
+  def sum_purchases_in_category(cat)
+    Purchase.where(subcategory: cat.subcategories).to_a.map(&:amount).sum
   end
 end
