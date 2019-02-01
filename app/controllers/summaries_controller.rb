c5ad7b6733a19031f72d34fe0ae78385193e2aa0
steviekaye <stevie.kaye@gmail.com>
2019-02-01 13:55:40 +1300

    Stacked column chart initial implementation

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -2,8 +2,7 @@ class SummariesController < ApplicationController
   def index
     @totals = category_totals
     @overall_sum = Purchase.all.to_a.map(&:amount).sum
-    @testthisout = category_totals_per_month
-    byebug
+    @month_totals = sum_month(category_totals_per_month.flatten.group_by { |n| n[:name] })
   end
 
   private
@@ -23,15 +22,21 @@ class SummariesController < ApplicationController
 
   def category_totals_per_month
     Category.all.map do |c|
-      [
-        c.name,
-        sum_purchases_by_month(c)
-      ]
+      sum_purchases_by_month(c).map do |p|
+        {
+          name: c.name,
+          date: p[:date],
+          amount: p[:amount]
+        }
+      end
     end
-    # do stuff here
   end
 
   def sum_purchases_by_month(cat)
     Purchase.where(subcategory: cat.subcategories).map { |p| { date: p.date, amount: p.amount } }
+  end
+
+  def sum_month(mon)
+    mon.map { |key, value| { name: key, data: value.group_by_month { |n| n[:date] }.map { |key, value| [key.strftime('%B %Y'), value.map { |v| v[:amount] }.sum] } } }
   end
 end
