e7e60ba31704138d517937a4e688f2828f628e0a
steviekaye <stevie.kaye@gmail.com>
2019-02-02 14:24:39 +1300

    Tweaked stacked bar chart to provide values "empty" months. Added pry to gemfile for better testing. Added specs for shape of data.

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development, :test do
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'capybara'
   gem 'factory_bot_rails'
+  gem 'rails-controller-testing'
   gem 'rspec-rails'
 end
 
@@ -56,6 +57,7 @@ group :development do
   gem 'listen', '>= 3.0.5', '< 3.2'
   gem 'web-console', '>= 3.3.0'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
+  gem 'pry'
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -2,7 +2,7 @@ class SummariesController < ApplicationController
   def index
     @totals = category_totals
     @overall_sum = Purchase.all.to_a.map(&:amount).sum
-    @month_totals = sum_month(category_totals_per_month.flatten.group_by { |n| n[:name] })
+    @month_totals = month_totals
   end
 
   private
@@ -16,8 +16,15 @@ class SummariesController < ApplicationController
     end
   end
 
-  def sum_purchases_in_category(cat)
-    Purchase.where(subcategory: cat.subcategories).to_a.map(&:amount).sum
+  def sum_purchases_in_category(category)
+    Purchase.where(subcategory: category.subcategories).to_a.map(&:amount).sum
+  end
+
+  def month_totals
+    first_purchase = Purchase.all.order(:date).first[:date]
+    last_purchase = Purchase.all.order(:date).last[:date] # this could be Date.today rather than most recent purchase
+    date_range = (first_purchase..last_purchase).map { |d| d.strftime('%B %Y') }.uniq
+    sum_month(category_totals_per_month.flatten.group_by { |n| n[:name] }, date_range)
   end
 
   def category_totals_per_month
@@ -32,11 +39,19 @@ class SummariesController < ApplicationController
     end
   end
 
-  def sum_purchases_by_month(cat)
-    Purchase.where(subcategory: cat.subcategories).map { |p| { date: p.date, amount: p.amount } }
+  def sum_purchases_by_month(category)
+    Purchase.where(subcategory: category.subcategories).map { |p| { date: p.date, amount: p.amount } }
   end
 
-  def sum_month(mon)
-    mon.map { |key, value| { name: key, data: value.group_by_month { |n| n[:date] }.map { |key, value| [key.strftime('%B %Y'), value.map { |v| v[:amount] }.sum] } } }
+  def sum_month(month, date_range)
+    month.map { |key, value| { name: key, data: value.group_by_month { |n| n[:date] }.map { |key, value| [key.strftime('%B %Y'), value.map { |v| v[:amount] }.sum] } } }.each do |c|
+      contained_months = c[:data].map(&:first)
+      blank_months = date_range - contained_months
+      blanks_months.each { |b| insert_empty_month(c[:data], [b, 0]) }
+    end
+  end
+
+  def insert_empty_month(vals, empty_month)
+    vals.insert(vals.index { |instance| empty_month.first.to_date <= instance.first.to_date } || -1, empty_month)
   end
 end
