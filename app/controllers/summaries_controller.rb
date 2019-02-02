e9bf361dd6c73fc851b98baaff7f4cd49c3e6bac
steviekaye <stevie.kaye@gmail.com>
2019-02-03 11:13:05 +1300

    More accurate naming for summary data methods Shifted some code from blocks to inline

--- app/controllers/summaries_controller.rb
+++ app/controllers/summaries_controller.rb
@@ -23,30 +23,26 @@ class SummariesController < ApplicationController
   def month_totals
     first_purchase = Purchase.all.order(:date).first[:date]
     last_purchase = Purchase.all.order(:date).last[:date] # this could be Date.today rather than most recent purchase
-    date_range = (first_purchase..last_purchase).map { |d| d.strftime('%B %Y') }.uniq
-    sum_month(category_totals_per_month.flatten.group_by { |n| n[:name] }, date_range)
+    months_in_range = (first_purchase..last_purchase).map { |d| d.strftime('%B %Y') }.uniq
+
+    group_and_sum_purchases_by_category_and_month(purchases_by_category, months_in_range)
   end
 
-  def category_totals_per_month
-    Category.all.map do |c|
-      sum_purchases_by_month(c).map do |p|
-        {
-          name: c.name,
-          date: p[:date],
-          amount: p[:amount]
-        }
-      end
+  def purchases_by_category
+    totals = Category.all.map do |c|
+      extract_purchases_by_category(c).map { |p| { name: c.name, date: p[:date], amount: p[:amount] } }
     end
+    totals.flatten.group_by { |n| n[:name] }
   end
 
-  def sum_purchases_by_month(category)
+  def extract_purchases_by_category(category)
     Purchase.where(subcategory: category.subcategories).map { |p| { date: p.date, amount: p.amount } }
   end
 
-  def sum_month(month, date_range)
-    month.map { |key, value| { name: key, data: value.group_by_month { |n| n[:date] }.map { |key, value| [key.strftime('%B %Y'), value.map { |v| v[:amount] }.sum] } } }.each do |c|
+  def group_and_sum_purchases_by_category_and_month(purchases_by_category, months_in_range)
+    purchases_by_category.map { |key, value| { name: key, data: value.group_by_month { |n| n[:date] }.map { |key, value| [key.strftime('%B %Y'), value.map { |v| v[:amount] }.sum] } } }.each do |c|
       contained_months = c[:data].map(&:first)
-      blank_months = date_range - contained_months
+      blank_months = months_in_range - contained_months
       blank_months.each { |b| insert_empty_month(c[:data], [b, 0]) }
     end
   end
