f5bd03618aba023d9e09dd5e1ce3a0775ca0bfbf
steviekaye <stevie.kaye@gmail.com>
2019-02-05 17:59:51 +1300

    Altered various methods in data_formatter - consolidated some, refactored others.

--- app/services/data_formatter.rb
+++ app/services/data_formatter.rb
@@ -10,11 +10,7 @@ class DataFormatter
   end
 
   def category_totals_by_month
-    first_purchase = Purchase.all.order(:date).first[:date]
-    last_purchase = Purchase.all.order(:date).last[:date] # this could be Date.today rather than most recent purchase
-    months_in_range = (first_purchase..last_purchase).map { |d| d.strftime('%B %Y') }.uniq
-
-    sum_purchases_by_month(purchases_by_category, months_in_range)
+    sum_purchases_by_month(purchases_by_category)
   end
 
   private
@@ -24,18 +20,15 @@ class DataFormatter
   end
 
   def purchases_by_category
-    totals = Category.all.map do |c|
-      extract_purchases_by_category(c).map { |p| { name: c.name, date: p[:date], amount: p[:amount] } }
+    totals = Category.all.map do |category|
+      purchases = @purchases.where(subcategory: category.subcategories).map { |purchase| { date: purchase.date, amount: purchase.amount } }
+      purchases.map { |purchase| purchase.merge!(name: category.name) }
     end
 
     totals.flatten.group_by { |n| n[:name] }
   end
 
-  def extract_purchases_by_category(category)
-    @purchases.where(subcategory: category.subcategories).map { |p| { date: p.date, amount: p.amount } }
-  end
-
-  def sum_purchases_by_month(purchases_by_category, months_in_range)
+  def sum_purchases_by_month(purchases_by_category)
     summaries = purchases_by_category.map do |key, value|
       { name: key, data: value.group_by_month { |n| n[:date] }.map do |key, value|
         [key.strftime('%B %Y'), value.map do |v|
@@ -44,14 +37,22 @@ class DataFormatter
       end }
     end
 
-    summaries.each do |c|
-      contained_months = c[:data].map(&:first)
-      blank_months = months_in_range - contained_months
-      blank_months.each { |b| insert_empty_month(c[:data], [b, 0]) }
-    end
+    ensure_all_months_present(summaries)
   end
 
-  def insert_empty_month(vals, empty_month)
-    vals.insert(vals.index { |instance| empty_month.first.to_date <= instance.first.to_date } || -1, empty_month)
-  end
+  def ensure_all_months_present(summaries)
+    first_purchase = Purchase.minimum(:date)
+    last_purchase = Purchase.maximum(:date)
+    months_in_range = (first_purchase..last_purchase).map { |d| d.strftime('%B %Y') }.uniq
+
+    summaries.each do |summary|
+      month_totals = months_in_range.map do |month|
+        existing_total = summary[:data].detect { |s| s.first == month }
+
+        existing_total || [month, 0]
+      end
+
+      summary[:data] = month_totals
+    end
+   end
 end
